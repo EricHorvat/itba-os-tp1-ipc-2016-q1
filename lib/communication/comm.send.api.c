430f0d9bf87c66cf3a0e1baa5023b7877823e653
Martin Goffan <martin.goffan@me.com>
2016-04-14 10:48:21 -0300

    FIFO Creation

--- lib/communication/comm.send.api.c
+++ lib/communication/comm.send.api.c
@@ -2,8 +2,16 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "../../utils.h"
+
+#define PATH_PREFIX "/tmp/"
+#define FIFO_PERMS 0666
 
 /**
  * 
@@ -17,11 +25,28 @@ void send_data(void *data, unsigned int size, comm_addr_t *addr, comm_error_t *e
 }
 
 void send_data_async(void * data, unsigned int size, comm_addr_t *addr, comm_callback_t cb) {
-	comm_error_t *err = malloc(sizeof(comm_error_t));
+
+	comm_error_t *err;
+
+	int fd;
+	char *fifo = (char*)malloc(sizeof(PATH_PREFIX)+sizeof(data));
+
+	strcat(fifo, PATH_PREFIX);
+	strcat(fifo, addr->host);
+
+	mkfifo(fifo, FIFO_PERMS);
+
+	fd = open(fifo, O_WRONLY);
+	write(fd, "Hola", sizeof("Hola"));
+	close(fd);
+
+	unlink(fifo);
+
+	err = (comm_error_t*)NEW(comm_error_t);
 	err->code = 0;
 	err->msg = "Operacion Exitosa";
 
-	sleep(2);
+	sleep(20);
 
 	cb(err, addr, (char*)data);
 }
