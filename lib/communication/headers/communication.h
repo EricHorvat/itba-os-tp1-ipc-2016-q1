548549050138c42b7286f09d334af89b51a2821c
Martin Goffan <martin.goffan@me.com>
2016-04-13 22:47:47 -0300

    Added communication header

---
+++ lib/communication/headers/communication.h
@@ -0,0 +1,23 @@
+#ifndef __COMMUNICATION_H_
+#define __COMMUNICATION_H_
+
+#include "comm.addr.h"
+
+typedef enum {
+	CONNECTION_STATE_IDLE,
+	CONNECTION_STATE_OPEN,
+	CONNECTION_STATE_CLOSED
+} connection_state_t;
+
+typedef struct {
+	comm_addr_t addr;
+	connection_state_t status;
+} connection_t;
+
+
+comm_error_code_t comm_open(connection_t *comm);
+#include "comm.send.api.h"
+#include "comm.receive.api.h"
+comm_error_code_t comm_close(connection_t *address);
+
+#endif
