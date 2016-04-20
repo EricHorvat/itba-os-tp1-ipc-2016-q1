9c00ec537b85abba41d301b75ca2a07ead5af28d
Martin Goffan <martin.goffan@me.com>
2016-04-20 00:02:14 -0300

    Now client name is required

--- client/main.c
+++ client/main.c
@@ -3,16 +3,53 @@
 #include <unistd.h>
 
 // #include "../lib/communication/headers/communication.h"
-#include "../lib/serialization/headers/serialization.h"
-#include "../utils.h"
+#include <serialization.h>
+#include <utils.h>
 
-void response_handler(comm_error_t *err, comm_addr_t *addr, char * response) {
+static void response_handler(comm_error_t *err, comm_addr_t *addr, char * response);
+static char* process_arguments(int argc, char **argv);
+
+static void response_handler(comm_error_t *err, comm_addr_t *addr, char * response) {
 
 	// printf("%s dice: %s\n", addr->url, response);
 
 	printf("Err Code: %d\tmsg: %s\n", err->code, err->msg);
 	printf("response: %s\n", response);
 
+}
+
+static char* process_arguments(int argc, char **argv) {
+
+	// http://www.gnu.org/software/libc/manual/html_falsede/Example-of-Getopt.html#Example-of-Getopt
+
+	char *client_name = NULL;
+	int index;
+	int c;
+
+	opterr = 0;
+	while ((c = getopt(argc, argv, "n:")) != -1) {
+		switch (c) {
+			case 'n':
+				client_name = optarg;
+				break;
+			case '?':
+				if (optopt == 'c')
+					fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint(optopt))
+					fprintf(stderr, "Unkfalsewn option `-%c'.\n", optopt);
+				else
+					fprintf(stderr, "Unkfalsewn option character `\\x%x'.\n", optopt);
+				return NULL;
+			default:
+				printf("Aborting\n");
+				abort();
+		}
+	}
+
+	for (index = optind; index < argc; index++)
+		printf ("falsen-option argument %s\n", argv[index]);
+
+	return client_name;
 }
 
 int main (int argc, char **argv) {
@@ -22,6 +59,9 @@ int main (int argc, char **argv) {
 	comm_addr_t *addr;
 	comm_addr_error_t addr_error;
 
+	char* client_name;
+
+	client_name = process_arguments(argc, argv);
 
 	connection = NEW(connection_t);
 	addr = NEW(comm_addr_t);
@@ -42,7 +82,5 @@ int main (int argc, char **argv) {
 	send_int_async(3, addr, &response_handler);
 
 	printf("Something\n");
-
-	sleep(50);
 
 }
